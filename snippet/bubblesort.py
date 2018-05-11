6e61f0b3409a1f56cfa9a58b9e151edf733214d1
hustwz <hustwz@gmail.com>
2018-05-11 23:03:03 +0800

    Update bubblesort

--- snippet/bubblesort.py
+++ snippet/bubblesort.py
@@ -1,6 +1,6 @@
 def bubbleSort(seq):
     for i in range(0, len(seq)-1):
-        for j in range(i+1, len(seq)):
-            if seq[i] > seq[j]:
-                seq[i], seq[j] = seq[j], seq[i]
+        for j in range(0, len(seq)-1-i):
+            if seq[j] > seq[j+1]:
+                seq[j+1], seq[j] = seq[j], seq[j+1]
     return seq
